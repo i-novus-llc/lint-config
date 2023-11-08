873ff30701ace8c2611caa775c6052181d98931e
Konstantin Grigorchuk <kgrigorchuk@i-novus.ru>
2023-11-08 14:52:54 +0300

    rf

--- src/react.ts
+++ src/react.ts
@@ -17,43 +17,47 @@ const config: Linter.Config = {
         "react-hooks"
     ],
     rules: {
-        "react/jsx-indent-props": ["error", 4],
-        // Теперь не будет требовать дефолты для обязательных параметров
-        "react/require-default-props": ["off"],
-        // Теперь можно использовать any в propTypes
-        "react/forbid-prop-types": ["off"],
-        "react/prop-types": ["off"],
         // Запрет <a href="#"> или <a href="javascript:void(0)">
-        "jsx-a11y/anchor-is-valid": ["error", {"aspects": ["invalidHref"]}],
-        // Теперь при совпаднии значений htmlFor у элемента и id у инпута не будет ошибки 
-        "jsx-a11y/label-has-associated-control": ["error", {"required": {"some": ["nesting", "id"]}}],
-        // Выключаем, чтоб можно было использовать setState в componentDidUpdate
-        "react/no-did-update-set-state": ["off"],
-        // Из за эффекта всплытия можно использовать функции и классы до их объявления, но с переменными не так.
-        // Почему то работает тоько error и off, а ["error", { "functions": false, "classes": false }] - не воспринимается (
-        // Деструктуризация в параметрах функций
-        "react/destructuring-assignment": ["warn", "always", { "destructureInSignature": "always" }],
+        'jsx-a11y/anchor-is-valid': ['error', { 'aspects': ['invalidHref'] }],
         // Если есть onClick, то key events теперь не обязательны
-        "jsx-a11y/click-events-have-key-events": "off",
-        // Теперь можно вешать обработчики на статические элементы, а не только на кнопки, чекбоксы, поля и прочее
-        "jsx-a11y/no-static-element-interactions": "off",
+        'jsx-a11y/click-events-have-key-events': 'off',
+        // Теперь при совпадении значений htmlFor у элемента и id у инпута не будет ошибки
+        'jsx-a11y/label-has-associated-control': ['error', { 'required': { 'some': ['nesting', 'id'] } }],
         // Теперь в input можно передавать autoFocus
-        "jsx-a11y/no-autofocus": "off",
+        'jsx-a11y/no-autofocus': 'off',
+        // Теперь можно вешать обработчики на статические элементы, а не только на кнопки, чекбоксы, поля и прочее
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'react/button-has-type': ['error'],
+        // Из-за эффекта всплытия можно использовать функции и классы до их объявления, но с переменными не так.
+        // Почему-то работает только error и off, а ["error", { "functions": false, "classes": false }] - не воспринимается (
+        // Деструктуризация в параметрах функций
+        'react/destructuring-assignment': ['warn', 'always', { 'destructureInSignature': 'always' }],
+        // Теперь можно использовать any в propTypes
+        'react/forbid-prop-types': ['off'],
+        'react/function-component-definition': ['off'],
         // off правил. иначе падает ошибка: Definition for rule was not found
-        "react/jsx-curly-newline": "off", // TODO ["error", { "multiline": "require", "singleline": "forbid" }]
-        "react/state-in-constructor": "off",
-        "react/static-property-placement": "off",
-        "react/jsx-props-no-spreading": "off",
+        'react/jsx-curly-newline': 'off', // TODO ["error", { "multiline": "require", "singleline": "forbid" }]
+        'react/jsx-filename-extension': [2, { 'extensions': ['.jsx', '.tsx'] }],
+        'react/jsx-indent': ['error', 4, { 'checkAttributes': true, 'indentLogicalExpressions': true }],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-props-no-spreading': 'off',
+        'react/jsx-tag-spacing': ['error', {
+            'closingSlash': 'never',
+            'beforeSelfClosing': 'always',
+            'afterOpening': 'never',
+            'beforeClosing': 'never',
+        }],
+        // Выключаем, чтоб можно было использовать setState в componentDidUpdate
+        'react/no-did-update-set-state': ['off'],
+        'react/prop-types': ['off'],
+        'react/react-in-jsx-scope': ['off'],
+        // Теперь не будет требовать дефолты для обязательных параметров
+        'react/require-default-props': ['off'],
         // Убираем сортировку методов
-        "react/sort-comp": "off",
-        // Расширения файлов, которые могут содержать jsx
-        "react/jsx-filename-extension": [2, { "extensions": [".jsx", ".tsx"] }],
-        "react/jsx-indent": ["error", 4, { "checkAttributes": true, "indentLogicalExpressions": true }],
-        "react/jsx-tag-spacing": ["error", { "closingSlash": "never", "beforeSelfClosing": "always", "afterOpening": "never", "beforeClosing": "never" }],
-        "react/react-in-jsx-scope": ["off"],
-        "react/function-component-definition": ["off"],
-        "react/button-has-type": ["error"]
-    }
+        'react/sort-comp': 'off',
+        'react/state-in-constructor': 'off',
+        'react/static-property-placement': 'off',
+    },
 }
 
 module.exports = config
--- src/styles.ts
+++ src/styles.ts
@@ -5,17 +5,17 @@ const config = {
         'stylelint-order',
     ],
     rules: {
-        // Отступ в 4 пробела
-        indentation: 4,
+        'at-rule-no-unknown': null,
         // Следующие 2 правила запрещают однострочные CSS-классы
-        'block-opening-brace-newline-after': 'always',
         'block-closing-brace-newline-before': 'always',
-        // Запрет на использование & при формировании имени класса: &-focused {}
-        'scss/selector-no-union-class-name': true,
+        'block-opening-brace-newline-after': 'always',
+        // Отступ в 4 пробела
+        'indentation': 4,
+        'scss/at-rule-no-unknown': true,
         // Не использовать & в селекторе, если он не нужен: & a {}
         'scss/selector-no-redundant-nesting-selector': true,
-        'at-rule-no-unknown': null,
-        'scss/at-rule-no-unknown': true,
+        // Запрет на использование & при формировании имени класса: &-focused {}
+        'scss/selector-no-union-class-name': true,
     },
 }
 
